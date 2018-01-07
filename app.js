8492b93bafcb36e9ad27c42b128b18b91f51eb8d
chanoch <email@chanoch.com>
2018-01-07 10:29:40 +0000

    Fix error handling.
    Added error page
    Fixed error handling to point to error page
    Fixed helmet handling
    Added compression (but not active as is blocking app.

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
+var compression = require('compression');
+
 var ErrorHandler = require('express-error-handler');
 
 var index = require('./routes/index');
 
 var app = express();
+// app.use(compression);
 
 app.use(favicon(path.join(__dirname, 'public', '/assets/img/va_favicon.png')));
 app.use(logger('dev'));
-app.use(helmet);
+app.use(helmet());
 app.set('views', path.join(__dirname, '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,8 +34,8 @@ app.use(function(req, res, next) {
 
 var errorHandler = ErrorHandler({
   static: {
-    '500': 'error.html',
-    '400': 'error.html',
+    '500': './public/error.html',
+    '404': './public/error.html',
   }
 }) 
 
