c0fe4a86807024f382795b485e71b73a9f9bebba
chanoch <email@chanoch.com>
2018-01-17 08:00:30 +0000

    Replaced revolution with css hero slider.

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -9,20 +9,6 @@ $(document).ready(function($) {
         $(".main-nav nav").toggleClass("show-nav");
     });
 
-//  Revolution Slider
-
-    if( $(".rev-slider").length ){
-        $(".rev-slider").revolution({
-            sliderType:"standard",
-            sliderLayout:"hero",
-            delay:9000,
-            gridheight:650,
-            navigation: {
-                arrows:{enable:true}
-            }
-        });
-    }
-
 //  Smooth Scroll
 
     $('.main-nav a[href^="#"], a[href^="#"].scroll').on('click',function (e) {
@@ -35,76 +21,4 @@ $(document).ready(function($) {
             window.location.hash = target;
         });
     });
-
-//  Form Validation
-
-    $("#form-contact button").on("click", function(){
-        $("#form-contact").validate({
-            submitHandler: function() {
-                $.post("assets/external/contact.php", $("#form-contact").serialize(),  function(response) {
-                    $('#form-contact .form-status').html(response);
-                    $('#form-contact button').attr('disabled','true');
-                });
-                return false;
-            }
-        });
-    });
-
 });
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// On Load
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-$(window).load(function(){
-    var $equalHeight = $('.container');
-    for( var i=0; i<$equalHeight.length; i++ ){
-        equalHeight( $equalHeight );
-    }
-});
-
-$(window).resize(function(){
-    var $equalHeight = $('.container');
-    for( var i=0; i<$equalHeight.length; i++ ){
-        equalHeight( $equalHeight );
-    }
-});
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Functions
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function checkDate(id) {
-    var date = $("#" + id).data("date");
-    var hasEvent = $("#" + id).data("hasEvent");
-    $('#modal').modal();
-}
-
-function equalHeight(container){
-    var currentTallest = 0,
-        currentRowStart = 0,
-        rowDivs = new Array(),
-        $el,
-        topPosition = 0;
-
-    $(container).find('.equal-height').each(function() {
-        $el = $(this);
-        $($el).height('auto');
-        topPostion = $el.position().top;
-        if (currentRowStart != topPostion) {
-            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-                rowDivs[currentDiv].height(currentTallest);
-            }
-            rowDivs.length = 0; // empty the array
-            currentRowStart = topPostion;
-            currentTallest = $el.height();
-            rowDivs.push($el);
-        } else {
-            rowDivs.push($el);
-            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-        }
-        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-            rowDivs[currentDiv].height(currentTallest);
-        }
-    });
-}
