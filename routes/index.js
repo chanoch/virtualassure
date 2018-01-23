8f2366fb9abbed927a453240aa232196bfdf259c
chanoch <email@chanoch.com>
2018-01-23 23:51:57 +0000

    Test sendgrid.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const sgMail = require('@sendgrid/mail');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('index');
+});
+
+router.get('/contact', function(req, res, next) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'chanoch@clearbrook.it',
+        from: 'jenita@virtualassure.co.uk',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    sgMail.send(msg);
 });
 
 module.exports = router;
