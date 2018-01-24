3a4a275174755a0534160c4ec37e7c5498115411
chanoch <email@chanoch.com>
2018-01-24 00:08:51 +0000

    Remove sendgrid - is erroring?

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const sgMail = require('@sendgrid/mail');
+// const sgMail = require('@sendgrid/mail');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,15 +8,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/contact', function(req, res, next) {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-        to: 'chanoch@clearbrook.it',
-        from: 'jenita@virtualassure.co.uk',
-        subject: 'Sending with SendGrid is Fun',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-    sgMail.send(msg);
+    console.log(process.env.SENDGRID_API_KEY);
+    // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    // const msg = {
+    //     to: 'chanoch@clearbrook.it',
+    //     from: 'jenita@virtualassure.co.uk',
+    //     subject: 'Sending with SendGrid is Fun',
+    //     text: 'and easy to do anywhere, even with Node.js',
+    //     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    // };
+//    sgMail.send(msg);
 });
 
 module.exports = router;
