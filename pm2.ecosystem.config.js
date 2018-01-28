b55ea7b670f423326215d8b3f159c21bc1312bfb
chanoch <email@chanoch.com>
2018-01-28 23:00:52 +0000

    Fixed syntax error in pm2 config.

--- pm2.ecosystem.config.js
+++ pm2.ecosystem.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-    app: {
-        name: "virtualassure",
-        script: "./index.js",
-        watch: true,
-        env: {
-            "NODE_ENV": "development"            
-        },
-        env_production: {
-            "NODE_ENV": "production"
+    apps: [
+        {
+            name: "virtualassure",
+            script: "./index.js",
+            watch: true,
+            env: {
+                "NODE_ENV": "development",
+            },
+            env_production: {
+                "NODE_ENV": "production"
+            }
         }
-    }
+    ]
 }
+
