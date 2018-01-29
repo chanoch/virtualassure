e3468662e21b5364fb9f9ee89fefa599df27dad8
chanoch <email@chanoch.com>
2018-01-29 00:26:38 +0000

    Fixing naive axios call to localhost

--- react/contact/AjaxRecaptchaForm.jsx
+++ react/contact/AjaxRecaptchaForm.jsx
@@ -82,7 +82,7 @@ export default class AjaxRecaptchaForm extends React.Component {
             return Object.assign(previous, fieldSetter());
         }, {});
         Object.assign(fields, {'g-recaptcha-response': this.recaptcha.getResponse()});
-        axios.post('http://localhost:3000/contact', fields)
+        axios.post(`${window.location.protocol}://${window.location.hostname}:${window.location.port}/contact`, fields)
             .then((response) => {
                 this.setState({phase: 'submitted'});
             }).catch((error) => {
