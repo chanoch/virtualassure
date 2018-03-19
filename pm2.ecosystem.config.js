4171491d8eb464d14abb016c3d19a6176e6314b8
Chanoch <chanoch@clearbrook.it>
2018-03-19 16:53:05 +0000

    Updated cv.

--- pm2.ecosystem.config.js
+++ pm2.ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "virtualassure",
-            script: "./index.js",
+            script: "./dist/server/index.js",
             watch: true,
             env: {
                 "NODE_ENV": "development",
